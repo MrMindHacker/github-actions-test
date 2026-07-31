d68b5421168684779de18bac758ccb1d18bc1116
sonarkeyur0092@gmail.com <keyur@learn.com>
2026-08-01 00:30:20 +0530

    changed exception message

--- python/math_operations.py
+++ python/math_operations.py
@@ -4,6 +4,6 @@ def sum_all(*args: float | int) -> float | int:
     """
     # Checking for non-numeric types to raise a clear error
     if not all(isinstance(x, (int, float)) for x in args):
-        raise TypeError("All arguments must be integers or floats")
+        raise TypeError("All arguments must be integers or floats! understood?")
         
     return sum(args)
